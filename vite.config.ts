c7c30dbf5119e7eff2f8f9d5f5f895323d5368a7
Mrityunjay <jhamrityunjay1234@gmail.com>
2026-03-11 18:20:02 +0530

    fix: changed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dentist-web-page/',   // 👈 ADD THIS
+  base: '/',   // 👈 ADD THIS
 
   plugins: [
     react(),
